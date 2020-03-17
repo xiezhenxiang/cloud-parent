0262a4323358be880a69ac50aae9cb1ab0502b47
解振翔 <1430742291@qq.com>
2020-03-17 14:20:29 +0800

    init

---
+++ cloud-demo/src/main/java/indi/shine/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package indi.shine.demo;
+
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
